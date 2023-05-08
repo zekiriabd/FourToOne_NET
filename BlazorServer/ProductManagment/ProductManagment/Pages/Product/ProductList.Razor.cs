9178e6735780c14fa4a0bbce553a588d3d51d25d
zekir <zekiriabd@hotmail.com>
2023-05-08 20:55:10 +0200

    Blazor

---
+++ BlazorServer/ProductManagment/ProductManagment/Pages/Product/ProductList.Razor.cs
@@ -0,0 +1,48 @@
+using ProductManagment.Models;
+
+namespace ProductManagment.Pages.Product
+{
+    public partial class ProductList 
+    {
+        public List<ProductModel> Products { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Products = new List<ProductModel>() {
+                new ProductModel
+                {
+                Id = 1,
+                Name = "Nike Air Max ",
+                Image = "https://cdn11.bigcommerce.com/s-ktikayh2p6/images/stencil/original/products/475/20955/baskets-de-jogging-a-coussin-dair__55328.1665613384.jpg?c=1",
+                Comment = @"Confort rembourré
+                        La semelle intermédiaire en mousse et l'unité Max Air au talon amortissent votre pied pour
+                        un confort durable. Le col en mousse protège votre cheville à chacun de vos mouvements.",
+                Price = 109.99M,
+                Discount = 50,
+                },
+                 new ProductModel
+                {
+                Id = 2,
+                Name = "Nike Air Max 2",
+                Image = "https://cdn11.bigcommerce.com/s-ktikayh2p6/images/stencil/original/products/475/20955/baskets-de-jogging-a-coussin-dair__55328.1665613384.jpg?c=1",
+                Comment = @"Confort rembourré
+                        La semelle intermédiaire en mousse et l'unité Max Air au talon amortissent votre pied pour
+                        un confort durable. Le col en mousse protège votre cheville à chacun de vos mouvements.",
+                Price = 109.99M,
+                Discount = 50,
+                },
+                  new ProductModel
+                {
+                Id = 3,
+                Name = "Nike Air Max 3",
+                Image = "https://cdn11.bigcommerce.com/s-ktikayh2p6/images/stencil/original/products/475/20955/baskets-de-jogging-a-coussin-dair__55328.1665613384.jpg?c=1",
+                Comment = @"Confort rembourré
+                        La semelle intermédiaire en mousse et l'unité Max Air au talon amortissent votre pied pour
+                        un confort durable. Le col en mousse protège votre cheville à chacun de vos mouvements.",
+                Price = 109.99M,
+                Discount = 50,
+                },
+            };
+        }
+    }
+}
--- BlazorServer/ProductManagment/ProductManagment/Program.cs
+++ BlazorServer/ProductManagment/ProductManagment/Program.cs
@@ -1,12 +1,18 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using ProductManagment.Data;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+
+
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
